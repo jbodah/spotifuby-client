c9d16a5e678854189198272b1a2edfb25ba269a8
Josh Bodah <jb3689@yahoo.com>
2015-08-28 13:26:36 -0400

    add test_helper

--- test/bot_test.rb
+++ test/bot_test.rb
@@ -1,11 +1,4 @@
-require 'minitest/autorun'
-require 'minitest/spec'
-require 'minitest/pride'
-require 'spy'
-require 'mocha/mini_test'
-require 'ostruct'
-
-$LOAD_PATH.push 'lib', __FILE__
+require 'test_helper'
 require 'spotifuby/client'
 require 'spotifuby/bot'
 
---
+++ test/test_helper.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/pride'
+require 'spy'
+require 'mocha/mini_test'
+require 'ostruct'
+
+$LOAD_PATH.push 'lib', __FILE__
